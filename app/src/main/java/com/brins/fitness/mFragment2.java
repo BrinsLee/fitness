270f8148d145b704e8424f30435cb8bc50ce33e0
Brin <747127514@qq.com>
2018-11-03 13:49:13 +0800

    第一阶段

--- app/src/main/java/com/brins/fitness/mFragment2.java
+++ app/src/main/java/com/brins/fitness/mFragment2.java
@@ -17,31 +17,31 @@ import static org.litepal.LitePalApplication.getContext;
 
 public class mFragment2 extends Fragment {
 
-    private ImageView imageView2;
+
     private WaveDrawable chromeWave;
     private TextView tv;
-    final int INIT=4000;
+    //final int INIT=4000;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.fragment_group,container,false);
-        imageView2 =  view.findViewById(R.id.iv);
+        //imageView2 =  view.findViewById(R.id.iv);
         tv=view.findViewById(R.id.text_count);
-        tv.setText(String.format("%s步",INIT));
+        //tv.setText(String.format("%s步",INIT));
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        chromeWave = new WaveDrawable(getContext(), R.drawable.pink);
+        /*chromeWave = new WaveDrawable(getContext(), R.drawable.pink);
         imageView2.setImageDrawable(chromeWave);
         chromeWave.setWaveAmplitude(30);
         //chromeWave.setWaveLength(30);
         chromeWave.setWaveSpeed(20);
         chromeWave.setWaveLength(500);
-        chromeWave.setLevel(INIT);
+        chromeWave.setLevel(INIT);*/
 
     }
 
